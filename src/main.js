de1a23b50080265a2315fd4e16f187ab14f10cf3
egor <makarov.e1999@yandex.ru>
2020-08-02 22:42:28 +0500

    fix module

--- src/main.js
+++ src/main.js
@@ -1,11 +1,12 @@
-`use strict`;
-
 const EVENTS_COUNT = 3;
 const mainTripBlock = document.querySelector(`.trip-main`);
 const mainTripFilterContainer = mainTripBlock.querySelector(`.trip-controls`);
 const mainTripFirstCaption = mainTripFilterContainer.querySelector(`h2.visually-hidden`);
 const eventsTripContainer = document.querySelector(`.trip-events`);
 const eventsTripCapture = eventsTripContainer.querySelector(`h2.visually-hidden`);
+const INSIDE_END_POS = `beforeend`;
+const OUTSIDE_END_POS = `afterend`;
+const OUTSIDE_BEGIN_POS = `afterbegin`;
 let eventsResultTemplate = ``;
 
 const renderElement = (parent, template, position) => {
@@ -336,19 +337,20 @@ const createEventListTemplate = () => {
 
   );
 };
-renderElement(mainTripBlock, createInfoAndCostTemplate(), `afterbegin`);
-renderElement(mainTripFirstCaption, createMainMenuTemplate(), `afterend`);
-renderElement(mainTripFilterContainer, createFiltersTemplate(), `beforeend`);
-renderElement(eventsTripCapture, createSortTemplate(), `afterend`);
-renderElement(eventsTripContainer, createFormMarksRoute(), `beforeend`);
-renderElement(eventsTripContainer, createDayListTemplate(), `beforeend`);
+renderElement(mainTripBlock, createInfoAndCostTemplate(), OUTSIDE_BEGIN_POS);
+renderElement(mainTripFirstCaption, createMainMenuTemplate(), OUTSIDE_END_POS);
+renderElement(mainTripFilterContainer, createFiltersTemplate(), INSIDE_END_POS);
+renderElement(eventsTripCapture, createSortTemplate(), OUTSIDE_END_POS);
+renderElement(eventsTripContainer, createFormMarksRoute(), INSIDE_END_POS);
+renderElement(eventsTripContainer, createDayListTemplate(), INSIDE_END_POS);
 const daysList = eventsTripContainer.querySelector(`.trip-days`);
-renderElement(daysList, createDayTemplate(), 'beforeend');
+renderElement(daysList, createDayTemplate(), INSIDE_END_POS);
 const dayEvents = daysList.querySelector(`.day`);
-renderElement(dayEvents, createEventListTemplate(), `beforeend`);
+renderElement(dayEvents, createEventListTemplate(), INSIDE_END_POS);
 const eventsList = dayEvents.querySelector(`.trip-events__list`);
 for (let i = 0; i < EVENTS_COUNT; i++) {
-  eventsResultTemplate+=createRoutePointTemplate();
+  eventsResultTemplate += createRoutePointTemplate();
 }
 
-renderElement(eventsList, eventsResultTemplate, `beforeend`);
+renderElement(eventsList, eventsResultTemplate, INSIDE_END_POS);
+export {renderElement as render};
